48f9f0db8f0731730ddb39d63afad713df80925f
Felix Galaup <felixgalaup@yahoo.fr>
2023-03-30 19:14:46 +0200

    [^](ex05).

--- ex05/Readme.md
+++ ex05/Readme.md
@@ -1,8 +1,8 @@
-# Exercise 5
+# Exercise 05
 
 See source to get more information.
 
 ## Sources
  - [Create char devices](https://tldp.org/LDP/lkmpg/2.6/html/x569.html)
  - [Misc device driver](https://embetronicx.com/tutorials/linux/device-drivers/misc-device-driver/)
- - [Linux divice operations](https://embetronicx.com/tutorials/linux/device-drivers/linux-device-driver-tutorial-programming/)
+ - [Linux device operations](https://embetronicx.com/tutorials/linux/device-drivers/linux-device-driver-tutorial-programming/)
--- ex05/main.c
+++ ex05/main.c
@@ -8,47 +8,59 @@
 #include <linux/err.h>       /* Needed for error handling */
 
 
+#define BUFFER_SIZE 128
+
+#define STUDENT_LOGIN "fgalaup"
+#define STUDENT_LOGIN_LENGTH 7
+
 // The write buffer
 uint8_t *write_buffer;
 
 /*
 ** This function will be called when we write the Misc Device file
 */
-static ssize_t fortytwo_write(struct file *file, const char __user *buf,
-               size_t len, loff_t *ppos)
+static ssize_t fortytwo_write(struct file *file, const char __user *buffer,
+               size_t length, loff_t *ppos)
 {
-    if (copy_from_user(write_buffer, buf, 128))
+    if (length != STUDENT_LOGIN_LENGTH)
+    {
+        pr_err("Error: Invalid value\n");
+        return (-EINVAL);
+    }
+
+    if (copy_from_user(write_buffer, buffer, BUFFER_SIZE))
         pr_err("Error while copying from user space.\n");
     pr_info("fortytwo misc device write\n");
-    if (memcmp(write_buffer, "fgalaup", 7))
+
+    if (memcmp(write_buffer, STUDENT_LOGIN, STUDENT_LOGIN_LENGTH))
     {
         pr_err("Error: Invalid value\n");
         return (-EINVAL);
     }   
-    return len; 
+    return length; 
 }
  
 /*
 ** This function will be called when we read the Misc Device file
 */
-static ssize_t fortytwo_read(struct file *filp, char __user *buf,
+static ssize_t fortytwo_read(struct file *filp, char __user *buffer,
                     size_t count, loff_t *f_pos)
 {
-    if (copy_to_user(buf, "fgalaup", 7))
+    size_t length = (count < STUDENT_LOGIN_LENGTH) ? count : STUDENT_LOGIN_LENGTH;
+
+    if (copy_to_user(buffer, STUDENT_LOGIN, length))
         pr_err("Error while copying to user space.\n");
     pr_info("fortytwo misc device read\n");
-    return 7;
+    return length;
 }
 
 /*
- * 
+ * This structure contains the methods implemented by misc device (file /dev/fortytwo)
  */
 static const struct file_operations fops = {
     .owner = THIS_MODULE,
     .read = &fortytwo_read,
     .write = &fortytwo_write,
-    .open = NULL,
-    .release = NULL,
 };
 
 
@@ -73,7 +85,7 @@ static int __init module_device_mics_fortytwo_start(void)
         return (error);
     }
 
-    if ((write_buffer = kmalloc(128, GFP_KERNEL)) == NULL)
+    if ((write_buffer = kmalloc(BUFFER_SIZE, GFP_KERNEL)) == NULL)
     {
         pr_err("Can't allocate memory for write buffer\n");
         misc_deregister(&fortytwo_device);
