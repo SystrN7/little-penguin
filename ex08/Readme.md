f881e8d124708330bcfa836c42725189294b196c
Felix Galaup <felixgalaup@yahoo.fr>
2023-03-30 14:45:11 +0200

    [+](ex08).

---
+++ ex08/Readme.md
@@ -0,0 +1,7 @@
+# Exercise 8
+
+I you want to see command i use to reformat the code please check the [Ex03 Readme.md](../ex03/Readme.md) file.
+
+## Sources
+ - [simple_write/read_from/to_buffer source code](https://elixir.bootlin.com/linux/v6.3-rc3/source/fs/libfs.c#L744)
+
--- ex08/source.c
+++ ex08/source.c
@@ -1,65 +1,67 @@
+// SPDX-License-Identifier: GPL-2.0-only
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
 #include <linux/slab.h>
-// Dont have a license, LOL
-MODULE_LICENSE("LICENSE");
+
+MODULE_LICENSE("GNU");
 MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
 MODULE_DESCRIPTION("Useless module");
-static ssize_t myfd_read
-(struct file *fp, char __user *user,
-size_t size, loff_t *offs);
-static ssize_t myfd_write(struct file *fp, const char __user *user,
-size_t size, loff_t *offs);
-static struct file_operations myfd_fops = {
-.owner = THIS_MODULE, .read = &myfd_read, .write = &myfd_write
-};
-static struct miscdevice myfd_device = {
-.minor = MISC_DYNAMIC_MINOR,.name = "reverse",
-.fops = &myfd_fops };
-char str[PAGE_SIZE];
-char *tmp;
-static int __init myfd_init
-(void) {
-int retval;
-retval = misc_register(&(*(&(myfd_device))));
-return 1;
-}
-static void __exit myfd_cleanup
-(void) {
-}
-ssize_t myfd_read
-(struct file *fp,
-char __user *user,
-size_t size,
-loff_t *offs)
+
+char reverse_string[PAGE_SIZE];
+char string[PAGE_SIZE];
+
+ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
 {
-size_t t, i;
-char *tmp2;
-/***************
-* Malloc like a boss
-***************/
-tmp2 = kmalloc(sizeof(char) * PAGE_SIZE * 2, GFP_KERNEL);
-tmp = tmp2;
-for (t = strlen(str) - 1, i = 0; t >= 0; t--, i++) {
-tmp[i] = str[t];
+	size_t string_length;
+	ssize_t t, i;
+
+	string_length = strlen(string);
+
+	// Reverse string
+	for (t = string_length - 1, i = 0; t >= 0; t--, i++)
+		reverse_string[i] = string[t];
+
+	reverse_string[i] = '\0';
+
+	return simple_read_from_buffer(user, size, offs, reverse_string, i);
 }
-tmp[i] = 0x0;
-return simple_read_from_buffer(user, size, offs, tmp, i);
+
+ssize_t myfd_write(struct file *fp, const char __user *user, size_t size,
+		   loff_t *offs)
+{
+	ssize_t result;
+
+	if (size + 1 > PAGE_SIZE) {
+		pr_err("String too long");
+		return -EINVAL;
+	}
+
+	result = simple_write_to_buffer(string, size, offs, user, size);
+	string[size] = '\0';
+
+	return result + 1;
 }
-ssize_t myfd_write
-(struct file *fp,
-const char __user *user,
-size_t size,
-loff_t *offs) {
-ssize_t res;
-res = 0;
-res = simple_write_to_buffer(str, size, offs, user, size) + 1;
-// 0x0 = ’\0’
-str[size + 1] = 0x0;
-return res;
+
+static struct file_operations myfd_fops = { .owner = THIS_MODULE,
+					    .read = &myfd_read,
+					    .write = &myfd_write };
+
+static struct miscdevice myfd_device = { .minor = MISC_DYNAMIC_MINOR,
+					 .name = "reverse",
+					 .fops = &myfd_fops };
+
+static int __init myfd_init(void)
+{
+	return misc_register(&myfd_device);
 }
+
+static void __exit myfd_cleanup(void)
+{
+	misc_deregister(&myfd_device);
+}
+
 module_init(myfd_init);
 module_exit(myfd_cleanup);
