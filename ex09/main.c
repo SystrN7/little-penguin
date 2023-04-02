5633a3c32557fdbe2d2a887155db9c1d73b5472a
Felix Galaup <felixgalaup@yahoo.fr>
2023-04-02 17:33:41 +0200

    [+](ex09)-wip 1.

--- ex09/main.c
+++ ex09/main.c
@@ -1,39 +1,66 @@
 #include <linux/kernel.h>       /* Needed for KERN_INFO */
 #include <linux/init.h>         /* Needed for the macros */
 #include <linux/module.h>       /* Needed by all modules */
-#include <linux/miscdevice.h>   /* Needed for misc device */
+#include <linux/proc_fs.h>      /* Needed for proc file system */
+#include <linux/mount.h>        /* Needed for mount structure */
 #include <linux/fs.h>       /* Needed for file operations */
 #include <linux/uaccess.h>      /* Needed for copy_form_user/copy_to_user */
 #include <linux/slab.h>         /* Needed for kmalloc */
-#include <linux/err.h>       /* Needed for error handling */
+#include <linux/list.h>         /* Needed for list_for_each_entry */
+#include <linux/string.h>       /* Needed for strcat */
+#include <linux/sched.h>        /* Needed for current */
+#include <linux/nsproxy.h>      /* Needed for nsproxy */
+#include <linux/mount.h>        /* Needed for mount */
+// #include <linux/dcache.h>       /* Needed for dentry */
 
 
+
+// #define BUFFER_SIZE 1024
+
+// Return the list of mounted filesystems
 static ssize_t mymounts_read(struct file *filp, char __user *buffer,
                     size_t count, loff_t *f_pos)
 {
     struct mnt_namespace *ns = current->nsproxy->mnt_ns;
     struct mount *mnt;
 
+    char *buf;
+    int len = 0;
+
     list_for_each_entry(mnt, &ns->list, mnt_list) {
-        printk(KERN_INFO "Mount point: %s\n", mnt->mnt_mountpoint->d_name.name);
+        len += strlen(mnt->mnt_mountpoint->d_name.name) + 1;
     }
+
+    buf = kmalloc(len, GFP_KERNEL);
+    if (!buf)
+        return -ENOMEM;
+    
+    list_for_each_entry(mnt, &ns->list, mnt_list) {
+
+        strcat(buf, mnt->mnt_mountpoint->d_name.name);
+        strcat(buf, " ");
+    }
+
+
+
+    return 0;
 }
 
-static const struct file_operations mymounts_fops = {
-    .owner = THIS_MODULE,
-    .read = &mymounts_read,
+static const struct proc_ops mymounts_fops = {
+    // .owner = THIS_MODULE,
+    .proc_read = &mymounts_read,
 };
 
 static int __init module_device_mymounts_start(void)
 {
-    int error;
+    // int error;
 
-    error = proc_create("mymounts", 0444, NULL, &mymounts_fops);
-    if (error)
-    {
-        pr_err("Can't register mymounts device\n");
-        return (error);
-    }
+    proc_create("mymounts", 0444, NULL, &mymounts_fops);
+    // if (error)
+    // {
+    //     pr_err("Can't register mymounts device\n");
+    //     return (error);
+    // }
 
     // if ((write_buffer = kmalloc(BUFFER_SIZE, GFP_KERNEL)) == NULL)
     // {
