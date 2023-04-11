8398e478f94e1e7dac9773b57e96421c0278d7c9
Felix Galaup <felixgalaup@yahoo.fr>
2023-04-11 23:46:52 +0200

    try thing.

--- ex09/Readme.md
+++ ex09/Readme.md
@@ -4,4 +4,5 @@ See source to get more information.
 
 ## Sources
  - [Create proc file char devices](https://embetronicx.com/tutorials/linux/device-drivers/procfs-in-linux/)
- - 
+ - [Linux Mounts header](https://elixir.bootlin.com/linux/v6.2.10/source/include/linux/mount.h)
+ - [Linux proc mounts file source](https://github.com/torvalds/linux/blob/master/fs/proc_namespace.c)
--- ex09/main.c
+++ ex09/main.c
@@ -2,155 +2,76 @@
 #include <linux/init.h>         /* Needed for the macros */
 #include <linux/module.h>       /* Needed by all modules */
 #include <linux/proc_fs.h>      /* Needed for proc file system */
-#include <linux/nsproxy.h>      /* Needed for nsproxy */
-#include <linux/mnt_namespace.h>
-#include <linux/security.h>
-#include <linux/fs_struct.h>
-#include <linux/sched/task.h>
-#include <linux/mount.h>
+#include <linux/mount.h>        /* Needed for mount structure */
+#include <linux/fs.h>           /* Needed for file operations */
+#include <linux/mount.h>        /* Needed for mount */
 
-#include <linux/seq_file.h>
+#include <linux/seq_file.h>     /* Needed for seq_file */
+#include <linux/fs_struct.h>    /* Needed for fs_struct */
 
-// #include <linux/proc_fs.h>
-// #include <linux/fs_struct.h>
+#include <linux/namei.h>        /* Needed for kern_path */
 
-static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
+// https://stackoverflow.com/questions/46591203/get-all-mount-points-in-kernel-module
+
+
+// struct vfsmount {
+// 	struct dentry *mnt_root;	/* root of the mounted tree */
+// 	struct super_block *mnt_sb;	/* pointer to superblock */
+// 	int mnt_flags;
+// 	struct mnt_idmap *mnt_idmap;
+// } __randomize_layout;
+
+
+static int mount_add(struct vfsmount *mount, void *arg)
 {
-	struct proc_mounts *p = m->private;
-	struct mount *r = real_mount(mnt);
-	struct path mnt_path = { .dentry = mnt->mnt_root, .mnt = mnt };
-	struct super_block *sb = mnt_path.dentry->d_sb;
-	int err;
+    struct seq_file *file = arg;
+	struct path mount_path = { .dentry = mount->mnt_root, .mnt = mount };
 
-	if (sb->s_op->show_devname) {
-		err = sb->s_op->show_devname(m, mnt_path.dentry);
-		if (err)
-			goto out;
-	} else {
-		mangle(m, r->mnt_devname ? r->mnt_devname : "none");
-	}
-	seq_putc(m, ' ');
-	/* mountpoints outside of chroot jail will give SEQ_SKIP on this */
-	err = seq_path_root(m, &mnt_path, &p->root, " \t\n\\");
-	if (err)
-		goto out;
-	seq_putc(m, ' ');
-	show_type(m, sb);
-	seq_puts(m, __mnt_is_readonly(mnt) ? " ro" : " rw");
-	err = show_sb_opts(m, sb);
-	if (err)
-		goto out;
-	show_mnt_opts(m, mnt);
-	if (sb->s_op->show_options)
-		err = sb->s_op->show_options(m, mnt_path.dentry);
-	seq_puts(m, " 0 0\n");
-out:
-	return err;
+	if (mount->mnt_sb->s_op->show_devname)
+		mount->mnt_sb->s_op->show_devname(file, mount->mnt_root);
+	else
+		seq_printf(file, "%s", mount->mnt_sb->s_id);
+	seq_putc(file, '\t');
+	seq_path(file, &mount_path, " \t\n\\");
+	seq_putc(file, '\n');
+
+    return 0;
 }
 
-static int mounts_open_common(struct inode *inode, struct file *file,
-			      int (*show)(struct seq_file *, struct vfsmount *))
+static int mymounts_show(struct seq_file *file, void *p)
 {
-	struct task_struct *task = get_proc_task(inode);
-	struct nsproxy *nsp;
-	struct mnt_namespace *ns = NULL;
-	struct path root;
-	struct proc_mounts *p;
-	struct seq_file *m;
-	int ret = -EINVAL;
+    struct path root;
+    struct vfsmount *mounts;
+    int error;
 
-	if (!task)
-		goto err;
+    error = kern_path("/", 0, &root);
+    if (error)
+        return error;
+    mounts = collect_mounts(&root);
+    if (IS_ERR(mounts))
+        return PTR_ERR(mounts);
 
-	task_lock(task);
-	nsp = task->nsproxy;
-	if (!nsp || !nsp->mnt_ns) {
-		task_unlock(task);
-		put_task_struct(task);
-		goto err;
-	}
-	ns = nsp->mnt_ns;
-	get_mnt_ns(ns);
-	if (!task->fs) {
-		task_unlock(task);
-		put_task_struct(task);
-		ret = -ENOENT;
-		goto err_put_ns;
-	}
-	get_fs_root(task->fs, &root);
-	task_unlock(task);
-	put_task_struct(task);
-
-	ret = seq_open_private(file, &mounts_op, sizeof(struct proc_mounts));
-	if (ret)
-		goto err_put_path;
-
-	m = file->private_data;
-	m->poll_event = ns->event;
-
-	p = m->private;
-	p->ns = ns;
-	p->root = root;
-	p->show = show;
-	INIT_LIST_HEAD(&p->cursor.mnt_list);
-	p->cursor.mnt.mnt_flags = MNT_CURSOR;
-
-	return 0;
-
- err_put_path:
-	path_put(&root);
- err_put_ns:
-	put_mnt_ns(ns);
- err:
-	return ret;
+    return iterate_mounts(&mount_add, file, mounts);
 }
 
-static int mounts_release(struct inode *inode, struct file *file)
+static int mymounts_open(struct inode *inode, struct file *file)
 {
-	struct seq_file *m = file->private_data;
-	struct proc_mounts *p = m->private;
-	path_put(&p->root);
-	mnt_cursor_del(p->ns, &p->cursor);
-	put_mnt_ns(p->ns);
-	return seq_release_private(inode, file);
+    return single_open(file, mymounts_show, NULL);
 }
 
-static int mounts_open(struct inode *inode, struct file *file)
-{
-	return mounts_open_common(inode, file, show_vfsmnt);
-}
-
-static __poll_t mounts_poll(struct file *file, poll_table *wait)
-{
-	struct seq_file *m = file->private_data;
-	struct proc_mounts *p = m->private;
-	struct mnt_namespace *ns = p->ns;
-	__poll_t res = EPOLLIN | EPOLLRDNORM;
-	int event;
-
-	poll_wait(file, &p->ns->poll, wait);
-
-	event = READ_ONCE(ns->event);
-	if (m->poll_event != event) {
-		m->poll_event = event;
-		res |= EPOLLERR | EPOLLPRI;
-	}
-
-	return res;
-}
-
-const struct file_operations proc_mounts_operations = {
-	.open		= mounts_open,
-	.read_iter	= seq_read_iter,
-	.splice_read	= generic_file_splice_read,
-	.llseek		= seq_lseek,
-	.release	= mounts_release,
-	// .poll		= mounts_poll,
+static const struct proc_ops mymounts_fops = {
+    // .owner = THIS_MODULE,
+    .proc_open = &mymounts_open,
+    .proc_read = &seq_read,
 };
 
 static int __init module_device_mymounts_start(void)
 {
+    // int error;
+
     proc_create("mymounts", 0444, NULL, &mymounts_fops);
+
+
     printk(KERN_INFO "Module loaded\n");
     return (0);
 }
